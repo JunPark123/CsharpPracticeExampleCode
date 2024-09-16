3546d94f972efd77c4ee41dd27801bb2ee1d474b
jpark <jpark@hubis.co.kr>
2024-09-16 21:22:38 +0900

    초기 커밋.

---
+++ Section02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i<11; i++)
+            {
+                double meter = FeetToMeter(i);
+                Console.WriteLine($"{i} ft = {meter:F4}");
+            }
+            Console.ReadLine();
+        }
+
+        static double FeetToMeter( int feet)
+        {
+            return feet * 0.3048;
+        }
+    }
+}
