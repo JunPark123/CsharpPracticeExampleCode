ba37d24045336ff5ddfb2a0ab7c49ee7c1ca7c09
jpark <jpark@hubis.co.kr>
2024-09-17 23:16:35 +0900

    연습문제3단원 풀고 넘어가기

---
+++ Section03_3-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section03_3_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48};
+
+            var findnum = numbers.Exists(n => n %8 == 0 && n % 9 == 0);
+            Console.WriteLine($"8과9로 나누어 떨어지는 수 찾기 => {findnum}");
+            Console.ReadLine();
+
+        }
+    }
+}
