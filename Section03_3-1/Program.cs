c74fa4bada416128cde38d73dae812cb53183a94
jpark <jpark@hubis.co.kr>
2024-09-18 12:44:04 +0900

    연습문제 풀이 완료

--- Section03_3-1/Program.cs
+++ Section03_3-1/Program.cs
@@ -14,6 +14,24 @@ namespace Section03_3_1
 
             var findnum = numbers.Exists(n => n %8 == 0 && n % 9 == 0);
             Console.WriteLine($"8과9로 나누어 떨어지는 수 찾기 => {findnum}");
+
+            Console.WriteLine($"ForEach로 요소들 2로 나누기");
+            numbers.ForEach(n=>Console.WriteLine(n/2));
+
+            Console.WriteLine($"LinQ로 50이상인 요소 열거하기");
+            IEnumerable<int> query= numbers.Where(n => n >= 50);
+            foreach(var v in query)
+            {
+                Console.WriteLine(v);
+            }
+
+            Console.WriteLine($"LinQ로 4번 풀기.길어서 생략");
+            List<int> numbs = numbers.Select(n => n * 2).ToList();
+            foreach (var v in numbs)
+            {
+                Console.WriteLine(v);
+            }
+
             Console.ReadLine();
 
         }
---
+++ Section03_3-2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section03_3_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string> { "Seoul", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong" };
+
+            var line = Console.ReadLine();
+            var index = names.FindIndex(s => s == line);
+            Console.WriteLine("입력된 값과 동일한 값 찾기");
+            Console.WriteLine(index);
+
+
+            
+            Console.WriteLine($"소문자 찾기 {names.Count(n => n.Contains('o'))}");
+
+            Console.WriteLine("o가 포함된 도시 찾고 배열로 저장하기");
+            IEnumerable<string> cities = names.Where(s => s.Contains('o')).ToArray();
+            foreach(var c in cities)
+            {
+                Console.WriteLine(c);
+            }
+
+            Console.WriteLine("마지막 문제.길어서 생략");
+            IEnumerable<int> asdf = names.Where(s => s.FirstOrDefault()=='B').Select(s => s.Length).ToArray();
+            foreach (var c in asdf)
+            {
+                Console.WriteLine(c);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
