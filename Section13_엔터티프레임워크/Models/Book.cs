dc23c8593787e09396b00c595907043a63161ac4
jpark <jpark@hubis.co.kr>
2024-11-07 22:35:09 +0900

    엔터티프레임워크 연습

---
+++ Section13_엔터티프레임워크/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section13_엔터티프레임워크.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int PublishedYear {  get; set; }
+        public virtual Author Author { get; set; }
+    }
+}
---
+++ Section16_비동기병렬프로그래밍/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace Section16_비동기병렬프로그래밍
+{
+   
+
+    internal class Program
+    {
+        private BackgroundWorker _worker = new BackgroundWorker();
+
+        
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
